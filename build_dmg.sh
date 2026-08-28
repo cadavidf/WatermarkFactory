b83c92a3d92f238a3a3e2ef407a6c6a001626b93
zentermind <hello@zentermind.com>
2026-08-28 17:17:52 -0500

    Keep DMG build output local

--- build_dmg.sh
+++ build_dmg.sh
@@ -5,14 +5,15 @@ APP_NAME="WatermarkFactory"
 SCHEME="WatermarkFactory"
 CONFIG="Release"
 DIST_DIR="dist"
+DERIVED_DATA="build/DerivedData"
 STAGE="$(mktemp -d "${TMPDIR:-/tmp}/watermarkfactory.XXXXXX")"
 trap 'rm -rf "$STAGE"' EXIT
 
 rm -rf "$DIST_DIR"
 mkdir -p "$DIST_DIR"
-xcodebuild -scheme "$SCHEME" -configuration "$CONFIG" build
+xcodebuild -scheme "$SCHEME" -configuration "$CONFIG" -derivedDataPath "$DERIVED_DATA" build
 
-APP_PATH="$(xcodebuild -scheme "$SCHEME" -configuration "$CONFIG" -showBuildSettings | awk -F' = ' '/^[[:space:]]*BUILT_PRODUCTS_DIR = /{dir=$2} END{print dir}')/$APP_NAME.app"
+APP_PATH="$(xcodebuild -scheme "$SCHEME" -configuration "$CONFIG" -derivedDataPath "$DERIVED_DATA" -showBuildSettings | awk -F' = ' '/^[[:space:]]*BUILT_PRODUCTS_DIR = /{dir=$2} END{print dir}')/$APP_NAME.app"
 cp -R "$APP_PATH" "$STAGE/"
 ln -s /Applications "$STAGE/Applications"
 
